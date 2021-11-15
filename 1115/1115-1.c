8848571184a8da557b309d535487b07f6d61f595
Your Name <biau235911@gmail.com>
2021-11-15 11:58:52 +0800

    add 1115

--- 1115/1115-1.c
+++ 1115/1115-1.c
@@ -3,11 +3,19 @@
 #define SIZE 50
 
 int main(void){
-    int a = 0, temp, seed, arr[SIZE];
+    int a = 0, temp, seed, arr[SIZE], result[42];
     scanf("%d",&seed);
     srand(seed);
+    for (int m = 0; m < 42; m++){
+        result[m]=1;
+    }
     for (int n = 0; n < 6; n++){
         a = rand() % 42 + 1;
+        if (result[a - 1] == 0){
+            n -= 1;
+            continue;
+        }
+        result[a-1] = 0;
         arr[n] = a;
     }
     for (int i = 0; i < 6; i++){
