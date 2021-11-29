d26a0791bb61272dbcc3c89f953c3d83380733ba
Your Name <biau235911@gmail.com>
2021-11-29 21:08:06 +0800

    add 1129

--- 1129/1129-1.c
+++ 1129/1129-1.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 
-int main(void){
-    int c;
-    int*aptr,*bptr;
+int main(void)
+{
+    char c, alph[26];
+    int i;
+    char *ptr = alph;
     scanf("%c", &c);
-    
+    if (c == 'a')
+    {
+        for (i = 0; i < 26; i++)
+        {
+            *ptr = i + 'a';
+            ptr++;
+        }
+    }
+    else
+    {
+        for (i = 0; i < 26; i++)
+        {
+            *ptr = i + 'A';
+            ptr++;
+        }
+    }
+    ptr = alph;
+    for (i = 0; i < 26; i++)
+    {
+        printf("%c\t", *ptr);
+        ptr++;
+    }
+    printf("\n");
 }
