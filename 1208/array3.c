3291f89b4c5158a4fa0fa2617c9a102b8e2c339b
charlie235911 <biau235911@gmail.com>
2021-12-08 16:32:21 +0800

    add 1208

--- 1208/array3.c
+++ 1208/array3.c
@@ -2,25 +2,19 @@
 
 int main(void)
 {
-    int n, m, temp;
-    scanf("%2d%2d", &n, &m);
-    int a[n][m];
+    int n, temp;
+    scanf("%d", &n);
+    int a[n][n];
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
+        for (int j = 0; j < n; j++)
         {
             scanf("%d", &a[i][j]);
         }
     }
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < m; j++)
-        {
-            if (i == j)
-            {
-                temp += a[i][j];
-            }
-        }
+            temp += a[i][i];
     }
-    printf("%3d", temp);
+    printf("%d\n", temp);
 }
