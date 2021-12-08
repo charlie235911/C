f90fdaf534e6e07e68c9af9f7c0f58a5decf9ef5
charlie235911 <biau235911@gmail.com>
2021-12-08 16:33:38 +0800

    add 1208

---
+++ 1208/array4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, temp;
+    scanf("%d", &n);
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+            temp += a[i][n-i-1];
+    }
+    printf("%d\n", temp);
+}
